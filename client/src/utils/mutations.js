8a1052f57b4c3a50068ac058f8142747b0100155
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-05-05 14:22:29 -0500

    Adding code to mutations.js with LOGIN_USER, ADD_USER, ADD_OFFER, ADD_PURCHASE, REMOVE_OFFER mutations.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,79 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($firstName: String!, $lastName: String!, $company: String!, $username: String!, $email: String!, $password: String!, $userType: String!) {
+        addUser(firstName: $firstName, lastName: $lastName, company: $company, username: $username, email: $email, password: $password, userType: $userType) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_OFFER = gql`
+    mutation addOffer($palletQty: Int!, $price: Float!, $material: String!, $dimension: String!, $address: String!, $state: String!, $image: String) {
+        addOffer(palletQty: $palletQty, price: $price, material: $material, dimension: $dimension, address: $address, state: $state, image: $image) {
+            _id
+            seller
+            palletQty
+            price
+            material
+            dimension
+            address
+            state
+            offerStatus
+            image
+            dateCreated
+        }
+    }
+`;
+
+export const ADD_PURCHASE = gql`
+    mutation addPurchase($offerId: ID!) {
+        addPurchase(offerId: $offerId) {
+            _id
+            seller
+            palletQty
+            price
+            material
+            dimension
+            address
+            state
+            offerStatus
+            image
+            dateCreated
+        }
+    }
+`;
+
+export const REMOVE_OFFER = gql`
+    mutation removeOffer($offerId: ID!) {
+        removeOffer(offerId: $offerId) {
+            _id
+            seller
+            palletQty
+            price
+            material
+            dimension
+            address
+            state
+            offerStatus
+            image
+            dateCreated
+        }
+    }
+`;
