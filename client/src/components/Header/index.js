0b7acbc76c24b14332022eaf3028f893cb004b8a
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-05-11 12:24:09 -0500

    Adding code to index.html in public directory to use Google Fonts. Creating App.css file to start working on the application's style. Adding code to Header/index.js, importing several icons from react-icons. Adding code to index.css with font-family.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import { FaPallet, FaInfoCircle, FaTruck } from 'react-icons/fa'; 
+import { GoSignIn } from 'react-icons/go';
+import { GiArchiveRegister, GiFiles } from 'react-icons/gi';
+import { RiLogoutBoxRFill, RiFileEditFill } from 'react-icons/ri';
+import { BsFilePersonFill } from 'react-icons/bs';
+import { MdLocalOffer } from 'react-icons/md';
 
 const Header = () => {
     
     return (
         <header>
-            <h1>Broken Pallets</h1>
+            <h1><FaPallet /> Broken Pallets</h1>
             {Auth.loggedIn() ? (
                 <nav>
                     {Auth.getProfile().data.userType === 'Seller' ? (
                         <ul>
-                            <li><Link to="/me/myOffers">My Offers</Link></li>
-                            <li><Link to="/createOffer">Create Offer</Link></li>
-                            <li><Link to="/me">Profile</Link></li>
-                            <li><Link onClick={Auth.logout} to="/">Logout</Link></li>
+                            <li><Link to="/me/myOffers"><GiFiles /> My Offers</Link></li>
+                            <li><Link to="/createOffer"><RiFileEditFill /> Create Offer</Link></li>
+                            <li><Link to="/me"><BsFilePersonFill /> Profile</Link></li>
+                            <li><Link onClick={Auth.logout} to="/"><RiLogoutBoxRFill /> Logout</Link></li>
                         </ul>
                     ) : (
                         <ul>
-                            <li><Link to="/allOffers">All Offers</Link></li>
-                            <li><Link to="/me/myPurchases">My Purchases</Link></li>
-                            <li><Link to="/me">Profile</Link></li>
-                            <li><Link onClick={Auth.logout} to="/">Logout</Link></li>
+                            <li><Link to="/allOffers"><MdLocalOffer />All Offers</Link></li>
+                            <li><Link to="/me/myPurchases"><FaTruck />My Purchases</Link></li>
+                            <li><Link to="/me"><BsFilePersonFill /> Profile</Link></li>
+                            <li><Link onClick={Auth.logout} to="/"><RiLogoutBoxRFill /> Logout</Link></li>
                         </ul>
                     )}
                 </nav>
             ) : (
                 <nav>
                     <ul>
-                        <li><Link to="/">About</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/signup">Sign Up</Link></li>
+                        <li><Link to="/"><FaInfoCircle /> About</Link></li>
+                        <li><Link to="/login"><GoSignIn /> Login</Link></li>
+                        <li><Link to="/signup"><GiArchiveRegister /> Sign Up</Link></li>
                     </ul>
                 </nav>
             )}
